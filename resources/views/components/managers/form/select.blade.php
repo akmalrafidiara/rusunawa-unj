a054783fd7d2a06304d914c54f28599151c69914
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-04 14:15:01 +0700

    refactor: Improve card component class handling and clean up commented code

--- resources/views/components/managers/form/select.blade.php
+++ resources/views/components/managers/form/select.blade.php
@@ -3,7 +3,7 @@
 @if ($isLabel)
     <label class="block text-sm font-medium text-gray-700 mb-2">{{ $label }}</label>
 @endif
-<select wire:model="{{ $wireModel }}" class="w-full border rounded-md py-2 px-4">
+<select wire:model="{{ $wireModel }}" class="w-full border rounded-md py-2 px-4 pr-10   ">
     <option value="">Pilih {{ $label }}</option>
     @foreach ($options as $option)
         <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
--- resources/views/components/managers/ui/card.blade.php
+++ resources/views/components/managers/ui/card.blade.php
@@ -1,5 +1,21 @@
 <div class="bg-white dark:bg-zinc-900 rounded-lg shadow border dark:border-zinc-700 overflow-hidden">
-    <div {{ $attributes->merge(['class' => 'p-4']) }}>
+    <div {{ $attributes->class([
+        'p-4' => !str($attributes->get('class'))->contains('p-'),
+    ]) }}>
         {{ $slot }}
     </div>
 </div>
+
+
+{{-- <div class="bg-white dark:bg-zinc-900 rounded-lg shadow border dark:border-zinc-700 overflow-hidden">
+    <div {{ $attributes->merge([
+        'class' => collect([
+            'text-sm', // <-- class Wajib, tidak bisa diubah
+        ])->when(
+            ! str($attributes->get('class'))->contains('p-'),
+            fn ($classes) => $classes->push('p-4') // <-- padding default kalau tidak diset user
+        )->implode(' ')
+    ]) }}>
+        {{ $slot }}
+    </div>
+</div> --}}
