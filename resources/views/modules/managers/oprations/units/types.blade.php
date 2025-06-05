d359eb35be1800b81ade20e85214049f464c8ec5
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-05 23:15:37 +0700

    feat: Add new content views for announcements, contacts, contents, FAQ, and galleries; update unit types layout

--- resources/views/modules/managers/oprations/units/types.blade.php
+++ resources/views/modules/managers/oprations/units/types.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<x-layouts.managers :title="__('Tipe Unit')">
+    <div class="flex flex-col gap-6">
+        <!-- Page Title -->
+        <x-managers.ui.page-title title="Tipe Unit"
+            subtitle="Kelola data referensi tipe unit untuk digunakan dalam data Unit" />
+
+        {{-- Dynamic Content - User Management --}}
+        <livewire:managers.unit-type />
+    </div>
+</x-layouts.managers>
