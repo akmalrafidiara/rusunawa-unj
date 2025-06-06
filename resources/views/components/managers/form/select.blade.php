646ee64c5ab1aa4e50754d946a6ae50053115169
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-06 18:58:51 +0700

    refactor: Simplify select component by removing unused props and improving label handling

--- resources/views/components/managers/form/select.blade.php
+++ resources/views/components/managers/form/select.blade.php
@@ -1,4 +1,4 @@
-@props(['options', 'label', 'wireModel', 'isLabel' => true])
+@props(['options', 'label'])
 
 @php
     // Ambil wire:model sebagai name untuk error validation
@@ -9,7 +9,7 @@
 <div class="w-full">
     <select {{ $attributes->whereStartsWith('wire:model') }}
         class="bg-transparant dark:bg-transparent border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2 px-4 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option value="" disabled class="dark:bg-zinc-800">Pilih {{ $label }}</option>
+        <option value="" disabled class="dark:bg-zinc-800">{{ $label }}</option>
         @foreach ($options as $option)
             <option value="{{ $option['value'] }}" class="dark:bg-zinc-800">{{ $option['label'] }}</option>
         @endforeach
