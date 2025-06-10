ba1774d60c351f2f4f6b66b85ffe7314dd4cd770
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-10 10:40:07 +0700

    feat: Update rates view layout to enhance structure and clarity

--- resources/views/modules/managers/oprations/units/rates.blade.php
+++ resources/views/modules/managers/oprations/units/rates.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<x-layouts.managers :title="__('Rate Unit')">
+    <div class="flex flex-col gap-6">
+        <!-- Page Title -->
+        <x-managers.ui.page-title title="Rate Unit"
+            subtitle="Kelola data referensi rate harga unit untuk digunakan dalam data Unit" />
+
+        {{-- Dynamic Content - Unit Cluster --}}
+        <livewire:managers.unit-rate />
+    </div>
+</x-layouts.managers>
