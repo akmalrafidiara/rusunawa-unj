b6e0a1149e6f3c3aa01950c82fdde2711dded8fe
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-10 10:40:37 +0700

    feat: Enhance input component to support Rupiah formatting and remove debugging notes

--- resources/views/components/managers/form/input.blade.php
+++ resources/views/components/managers/form/input.blade.php
@@ -5,10 +5,8 @@
     'class' => 'w-full',
     'required' => false,
     'clearable' => false,
+    'rupiah' => false,
 ])
-
-{{-- !IMPORTANT NOTE --}}
-{{-- THIS COMPONENT UNDER DEBUGING, THIS COMPONENT CANT USING CLEARABLE FOR SHOWING CLEAR BUTTON --}}
 
 @php
     $wireModel = $attributes->whereStartsWith('wire:model')->first();
@@ -23,11 +21,30 @@
             </div>
         @endif
 
-        <input type="{{ $type }}" {{ $attributes->merge(['wire:model' => $wireModel]) }}
-            placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
-            class="block w-full border rounded-md {{ $error ? 'border-red-500' : 'border-gray-500' }} dark:placeholder-zinc-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 {{ $icon ? 'pl-10' : 'pl-4' }} pr-{{ $clearable ? '10' : '4' }} {{ $class }}">
+        @if ($rupiah)
+            <div x-data="{
+                raw: $wire.entangle('{{ $wireModel }}'),
+                display: '',
+                format() {
+                    this.display = this.raw ? new Intl.NumberFormat('id-ID').format(this.raw) : '';
+                },
+                parse(val) {
+                    const clean = val.replace(/[^0-9]/g, '');
+                    this.raw = clean;
+                    this.format();
+                }
+            }" x-init="format()" class="relative">
+                <span class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-300">Rp</span>
+                <input type="text" x-model="display" x-on:input="parse($event.target.value)"
+                    placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
+                    class="block w-full border rounded-md {{ $error ? 'border-red-500' : 'border-gray-500' }} dark:placeholder-zinc-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 {{ $icon || $rupiah ? 'pl-10' : 'pl-4' }} pr-{{ $clearable ? '10' : '4' }} {{ $class }}" />
+            </div>
+        @else
+            <input type="{{ $type }}" {{ $attributes->merge(['wire:model' => $wireModel]) }}
+                placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
+                class="block w-full border rounded-md {{ $error ? 'border-red-500' : 'border-gray-500' }} dark:placeholder-zinc-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 {{ $icon ? 'pl-10' : 'pl-4' }} pr-{{ $clearable ? '10' : '4' }} {{ $class }}" />
+        @endif
 
-        {{-- Tombol Clear --}}
         @if ($clearable && $wireModel)
             @if ($this->{$wireModel})
                 <button type="button" wire:click="$set('{{ $wireModel }}', null)"
