7be94dcc1d899899b838daca3839ee4f0af53f4d
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-10 21:37:14 +0700

    feat: Add export buttons for PDF and Excel in the button-export component

---
+++ resources/views/components/managers/ui/button-export.blade.php
@@ -0,0 +1,18 @@
+<div class="flex border rounded-md border-gray-500 text-black overflow-hidden self-stretch">
+    <!-- PDF Button -->
+    <button type="button" wire:click="exportPdf"
+        class="flex-1 flex gap-1 items-center justify-center border-none outline-none cursor-pointer text-xs px-4 hover:bg-red-600 hover:text-white h-full">
+        <span>PDF</span>
+        <flux:icon name="inbox-arrow-down" class="w-3 h-3" />
+    </button>
+
+    <!-- Divider -->
+    <div class="self-stretch w-[1px] bg-black"></div>
+
+    <!-- Excel Button -->
+    <button type="button" wire:click="exportExcel"
+        class="flex-1 flex gap-1 items-center justify-center border-none outline-none cursor-pointer text-xs px-4 hover:bg-green-600 hover:text-white h-full">
+        <span>Excel</span>
+        <flux:icon name="document-arrow-down" class="w-3 h-3" />
+    </button>
+</div>
