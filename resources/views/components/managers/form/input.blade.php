a4ae3e81cddd1f0fe86a70c55b9c68fbb3348cda
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-10 12:58:13 +0700

    fix: Update clearable button condition to check for actual value in input component

--- resources/views/components/managers/form/input.blade.php
+++ resources/views/components/managers/form/input.blade.php
@@ -11,6 +11,9 @@
 @php
     $wireModel = $attributes->whereStartsWith('wire:model')->first();
     $error = $wireModel && $errors->has($wireModel);
+
+    // THIS IS NOT A BUG, this is to get the actual wire:model value
+    $value = data_get($this, $wireModel);
 @endphp
 
 <div class="w-full">
@@ -45,13 +48,11 @@
                 class="block w-full border rounded-md {{ $error ? 'border-red-500' : 'border-gray-500' }} dark:placeholder-zinc-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 {{ $icon ? 'pl-10' : 'pl-4' }} pr-{{ $clearable ? '10' : '4' }} {{ $class }}" />
         @endif
 
-        @if ($clearable && $wireModel)
-            @if ($this->{$wireModel})
-                <button type="button" wire:click="$set('{{ $wireModel }}', null)"
-                    class="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 focus:outline-none z-10">
-                    <flux:icon name="x-mark" />
-                </button>
-            @endif
+        @if ($clearable && $wireModel && $value)
+            <button type="button" wire:click="$set('{{ $wireModel }}', null)"
+                class="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 focus:outline-none z-10">
+                <flux:icon name="x-mark" />
+            </button>
         @endif
     </div>
 
