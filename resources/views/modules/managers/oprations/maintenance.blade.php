bf9bd5e62e482fe63599618e5a343122827a1fc2
faizalrizqikholily <faizalrizqikholily26@gmail.com>
2025-06-12 08:43:05 +0700

    update routes halaman manager - manajamen operasional - laporan keluhan dan maintenance

--- resources/views/modules/managers/oprations/maintenance.blade.php
+++ resources/views/modules/managers/oprations/maintenance.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-</div>
+<x-layouts.managers :title="__('Maintenance')">
+    <div class="flex flex-col gap-6">
+        <!-- Page Title -->
+        <x-managers.ui.page-title title="Maintenance / Pemeliharaan"
+            subtitle="Kelola Maintenance atau Pemeliharaan yang dilakukan pada unit hunian" />
+    </div>
+</x-layouts.managers>
--- resources/views/modules/managers/oprations/reports-and-complaints.blade.php
+++ resources/views/modules/managers/oprations/reports-and-complaints.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+<x-layouts.managers :title="__('Laporan & Keluhan')">
+    <div class="flex flex-col gap-6">
+        <!-- Page Title -->
+        <x-managers.ui.page-title title="Laporan & Keluhan"
+            subtitle="Kelola Laporan dan Keluhan yang dibuat oleh penghuni" />
+    </div>
+</x-layouts.managers>
