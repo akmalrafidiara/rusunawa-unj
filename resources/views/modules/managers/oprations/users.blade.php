98bf8f6c662d05a66085f8392f056f0f9e56a3f6
Akmal Rafi <akmalrafidiara@gmail.com>
2025-06-13 20:30:29 +0700

    feat: add income reports, maintenance, and reports & complaints views; remove user management view

--- resources/views/modules/managers/oprations/users.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts.managers :title="__('User Management')">
-    <div class="flex flex-col gap-6">
-        <!-- Page Title -->
-        <x-managers.ui.page-title title="Manajemen Pengguna" subtitle="Kelola data pengguna sistem Rusunawa UNJ" />
-
-        {{-- Dynamic Content - User Management --}}
-        <livewire:managers.user-management />
-    </div>
-</x-layouts.managers>
