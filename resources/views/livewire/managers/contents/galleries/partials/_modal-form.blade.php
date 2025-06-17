cc60f5cbb132b73cee7ea00dbed752c56514f28a
faizalrizqikholily <faizalrizqikholily26@gmail.com>
2025-06-17 21:43:42 +0700

    feat : add filepond in upload  image at content management galleries

--- resources/views/livewire/managers/contents/galleries/partials/_modal-form.blade.php
+++ resources/views/livewire/managers/contents/galleries/partials/_modal-form.blade.php
@@ -19,12 +19,14 @@
             </div>
         @endif
 
-        <input type="file" wire:model="image" class="block w-full text-sm text-gray-500
-                file:mr-4 file:py-2 file:px-4
-                file:rounded-full file:border-0
-                file:text-sm file:font-semibold
-                file:bg-blue-50 file:text-blue-700
-                hover:file:bg-blue-100" />
+        <div wire:key="filepond-image-wrapper">
+            <x-filepond::upload
+            wire:model.live="image"
+            allow-multiple="false"
+            max-file-size="2MB"
+            accepted-file-types="image/jpeg,image/png,image/gif,image/webp"
+            />
+        </div>
 
         @error('image')
             <span class="text-red-500 text-sm">{{ $message }}</span>
