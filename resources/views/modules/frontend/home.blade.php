d248db97b59b87d0438e91119a94fe8295f151fc
faizalrizqikholily <faizalrizqikholily26@gmail.com>
2025-06-22 20:49:17 +0700

    feat : add background text on landing page "KAMAR"

--- resources/views/modules/frontend/home.blade.php
+++ resources/views/modules/frontend/home.blade.php
@@ -5,14 +5,25 @@
     </section>
 
     {{-- Form Kamar --}}
-    <div class="container mx-auto px-4 mt-8 **sm:-mt-38 md:-mt-46 lg:-mt-56** relative z-10">
+    <div class="container mx-auto px-4 mt-8 sm:-mt-38 md:-mt-46 lg:-mt-56 relative z-10">
         <div class="max-w-7xl mx-auto bg-white dark:bg-zinc-800 p-4 sm:p-6 rounded-lg shadow-lg w-full">
             <livewire:frontend.unit-avaibility-check-form mode="redirect" />
         </div>
     </div>
 
     {{-- Unit Types Section --}}
-    <section class="container mx-auto px-4 py-8">
-        <livewire:frontend.unit-type.index />
+    <section class="container mx-auto px-4 py-8 relative overflow-hidden">
+
+        {{-- Background "KAMAR" text --}}
+        <div class="absolute top-8 left-0 md:top-12 md:left-2 lg:top-1 lg:left-0 pointer-events-none z-[-1] opacity-10">
+            <span class="text-8xl md:text-[200px] lg:text-[300px] font-extrabold text-white
+                         [text-shadow:2px_2px_0_#9ca3af,-2px_-2px_0_#9ca3af,2px_-2px_0_#9ca3af,-2px_2px_0_#9ca3af,2px_0px_0_#9ca3af,-2px_0px_0_#9ca3af,0px_2px_0_#9ca3af,0px_-2px_0_#9ca3af]
+                         [white-space:nowrap]">KAMAR</span>
+        </div>
+
+        {{-- Render the Unit Type Listing component --}}
+        <div>
+            <livewire:frontend.unit-type.index />
+        </div>
     </section>
 </x-layouts.frontend>
